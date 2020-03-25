5500eeaeb60df4d9a59bf51d0758b0f43c9e1684
Sergey Litvinov <slitvinov@gmail.com>
2020-03-25 21:15:44 +0100

    add initial conditions for python

--- sheet/main.py
+++ sheet/main.py
@@ -4,11 +4,11 @@ import scipy.integrate
 import numpy
 import math
 
-N = 400
-M = 100
-L = 2
+n = 10
+eps = 0.5
+M = 10
+L = 1
 t1 = 4.0
-eps = 0.1
 
 def f(z, t):
     cos = math.cos
@@ -16,12 +16,12 @@ def f(z, t):
     cosh = math.cosh
     sinh = math.sinh
     pi = math.pi
-    x = z[:N]
-    y = z[N:]
-    fx = numpy.zeros(N)
-    fy = numpy.zeros(N)
-    for i in range(N):
-        for j in range(N):
+    x = z[:n]
+    y = z[n:]
+    fx = numpy.zeros(n)
+    fy = numpy.zeros(n)
+    for i in range(n):
+        for j in range(n):
             if i == j:
                 continue
             dx = x[i] - x[j]
@@ -29,20 +29,20 @@ def f(z, t):
             den = cosh(2*pi*dy) - cos(2*pi*dx) + eps*eps
             fx[i] -= sinh(2*pi*dy)/den
             fy[i] += sin(2*pi*dx)/den
-    return numpy.hstack((fx, fy))/(2.0*N)
+    return numpy.hstack((fx, fy))/(2.0*n)
 
-
-x0 = numpy.linspace(0, L, N)
-y0 = numpy.zeros(N)
-z0 = numpy.hstack((x0, y0))
+x0 = numpy.linspace(0, L, n)
+x = x0 + 0.01 * numpy.sin(2 * math.pi * x0)
+y = -0.01 * numpy.sin(2 * math.pi * x0)
+z0 = numpy.hstack((x, y))
 t0 = 0
 t = numpy.linspace(t0, t1, M)
 z, info = scipy.integrate.odeint(f, z0, t, full_output=True)
-x = z[:, :N]
-y = z[:, N:]
+x = z[:, :n]
+y = z[:, n:]
 
 for t in range(M):
     if t > 0:
         print("")
-    for i in range(N):
+    for i in range(n):
         print(x[t, i], y[t, i])
