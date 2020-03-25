b16a1531c3c76a590aa536952df396e6443088e0
Sergey Litvinov <slitvinov@gmail.com>
2020-03-25 18:21:25 +0100

    add parameters -n and -e

--- sheet/main.c
+++ sheet/main.c
@@ -4,91 +4,164 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_odeiv2.h>
 
-enum {N = 400};
-enum {M = 100};
-
 static char me[] = "sheet/main";
+static void
+usg(void)
+{
+    fprintf(stderr, "%s -n int > out\n", me);
+    exit(1);
+}
+
+enum { M = 10 };
 static const double pi = 3.141592653589793;
-static const double eps = 0.5;
 static const double L = 1.0;
 static const double t1 = 4.0;
+static const double epsrel = 1e-6;
+static const double epsabs = 0;
+
+struct Param {
+    int n;
+    double eps;
+};
 
 int
-func(double t, const double *z, double *f, void *params)
+func(double t, const double *z, double *f, void *params0)
 {
-  (void)(params);
-  (void)(t);
-  const double *x;
-  const double *y;
-  double *fx;
-  double *fy;
-  double dx;
-  double dy;
-  double den;
-  int i;
-  int j;
+    (void) (t);
+    const double *x;
+    const double *y;
+    double *fx;
+    double *fy;
+    double dx;
+    double dy;
+    double den;
+    double eps;
+    int i;
+    int j;
+    int n;
+    struct Param *params;
 
-  x = z;
-  y = &z[N];
-  fx = f;
-  fy = &f[N];
-  for (i = 0; i < N; i++)
-    fx[i] = fy[i] = 0;
-  for (i = 0; i < N; i++)
-    for (j = 0; j < N; j++)
-      if (i != j) {
-	dx = x[i] - x[j];
-	dy = y[i] - y[j];
-	den = cosh(2*pi*dy) - cos(2*pi*dx) + eps*eps;
-	fx[i] -= sinh(2*pi*dy)/den;
-	fy[i] += sin(2*pi*dx)/den;
-      }
-  for (i = 0; i < N; i++) {
-    fx[i] /= 2*N;
-    fy[i] /= 2*N;
-  }
-  return GSL_SUCCESS;
+    params = params0;
+    n = params->n;
+    eps = params->eps;
+    x = z;
+    y = &z[n];
+    fx = f;
+    fy = &f[n];
+    for (i = 0; i < n; i++)
+        fx[i] = fy[i] = 0;
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            if (i != j) {
+                dx = x[i] - x[j];
+                dy = y[i] - y[j];
+                den = cosh(2 * pi * dy) - cos(2 * pi * dx) + eps * eps;
+                fx[i] -= sinh(2 * pi * dy) / den;
+                fy[i] += sin(2 * pi * dx) / den;
+            }
+    for (i = 0; i < n; i++) {
+        fx[i] /= 2 * n;
+        fy[i] /= 2 * n;
+    }
+    return GSL_SUCCESS;
 }
 
 int
-main()
+main(int argc, char **argv)
 {
-  int i;
-  int j;
-  double t;
-  double z[2*N];
-  double *x;
-  double *y;
-  double h;
-  double ti;
-  double dt;
-  double x0;
-  gsl_odeiv2_driver * driver;
-  gsl_odeiv2_system sys = {func, NULL, 2*N, NULL};
-  driver = gsl_odeiv2_driver_alloc_y_new (&sys, gsl_odeiv2_step_rk4,
-				  1e-6, 1e-6, 0.0);
-  x = z;
-  y = &z[N];
-  h = L/(N - 1);
-  for (i = 0; i < N; i++) {
-    x0 = i*h;
-    x[i] = x0 + 0.01*sin(2*pi*x0);
-    y[i] = -0.01*sin(2*pi*x0);
-  }
-  dt = t1 / (M - 1);
-  t = 0;
-  for (i = 0; i < M; i++) {
-    fprintf(stderr, "%s: %d\n", me, i);
-    ti = dt * i;
-    if (gsl_odeiv2_driver_apply(driver, &t, ti, z) != GSL_SUCCESS) {
-      fprintf(stderr, "%s: driver failed\n", me);
+    (void) argc;
+    int i;
+    int j;
+    double t;
+    int n;
+    double *z;
+    double *x;
+    double *y;
+    double h;
+    double ti;
+    double dt;
+    double x0;
+    double eps;
+    gsl_odeiv2_driver *driver;
+    double dt_start;
+    gsl_odeiv2_system sys;
+    struct Param param;
+    int Nflag;
+    int Eflag;
+
+    Nflag = Eflag = 0;
+    while (*++argv != NULL && argv[0][0] == '-')
+        switch (argv[0][1]) {
+        case 'h':
+            usg();
+            break;
+        case 'n':
+            argv++;
+            if (argv[0] == NULL) {
+                fprintf(stderr, "%s: -n needs an argument\n", me);
+                exit(2);
+            }
+            n = atoi(argv[0]);
+            Nflag = 1;
+            break;
+        case 'e':
+            argv++;
+            if (argv[0] == NULL) {
+                fprintf(stderr, "%s: -e needs an argument\n", me);
+                exit(2);
+            }
+            eps = atof(argv[0]);
+            Eflag = 1;
+            break;	    
+        default:
+            fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
+            exit(2);
+        }
+    if (Nflag == 0) {
+        fprintf(stderr, "%s: -n is not given\n", me);
+        exit(2);
+    }
+    if (Eflag == 0) {
+        fprintf(stderr, "%s: -e is not given\n", me);
+        exit(2);
+    }
+    z = malloc(2 * n * sizeof(*z));
+    if (z == NULL) {
+      fprintf(stderr, "malloc failed\n", me);
       exit(2);
-      }
-    if (i > 0)
-      printf("\n");
-    for (j = 0; j < N; j++)
-      printf("%.16g %.16g\n", x[j], y[j]);
-  }
-  gsl_odeiv2_driver_free(driver);
-  return 0;
+    }
+    param.n = n;
+    param.eps = eps;
+    dt_start = 0.001;
+    sys.function = func;
+    sys.jacobian = NULL;
+    sys.dimension = 2 * n;
+    sys.params = &param;
+    driver = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rk4,
+                                           dt_start, epsrel, epsabs);
+    x = z;
+    y = &z[n];
+    h = L / (n - 1);
+    for (i = 0; i < n; i++) {
+        x0 = i * h;
+        x[i] = x0 + 0.01 * sin(2 * pi * x0);
+        y[i] = -0.01 * sin(2 * pi * x0);
+    }
+    dt = t1 / (M - 1);
+    t = 0;
+    for (i = 0; i < M; i++) {
+        ti = dt * i;
+        fprintf(stderr, "%s: %g\n", me, ti);
+        if (gsl_odeiv2_driver_apply(driver, &t, ti, z) != GSL_SUCCESS) {
+            fprintf(stderr, "%s: driver failed\n", me);
+            exit(2);
+        }
+        if (i > 0)
+            printf("\n");
+        for (j = 0; j < n; j++)
+            printf("%.16g %.16g\n", x[j], y[j]);
+    }
+    free(z);
+    gsl_odeiv2_driver_free(driver);
+    return 0;
 }
---
+++ sheet/main.gp
@@ -0,0 +1,4 @@
+i = 4
+set xrange [0:1]
+set yrange [-0.5:0.5]
+plot "q" every :::i::i w lp
