a651a814d36359d8abb0d7eefbb182ddcb8523e7
bianx <bianx@ethz.ch>
2020-03-26 20:45:55 +0100

    add play folder

---
+++ play/main.gp
@@ -0,0 +1,46 @@
+reset
+set terminal postscript eps
+i = 20
+j = 0
+
+t1=4
+M=20
+dt=4.0/20
+
+t=0
+
+n=1600
+eps="025"
+
+round(x) = x - floor(x) < 0.5 ? floor(x) : ceil(x)
+
+i=round(t/dt)
+print dt, i, t
+set size 1, 0.5
+set output "eps".eps."_time_".t.".eps"
+
+L=1
+L2=2
+set xrange [0:L2]
+set yrange [-0.275:0.275]
+
+set xlabel 'x'
+set ylabel 'y'
+set xtics 1
+set ytics 0.275
+wi=5
+si=0.5
+
+file_in="output_n".n."_eps".eps."_c.dat"
+
+print file_in
+plot \
+file_in every :::i::i u 1:2 w l lc 1 lw wi t 't='.t,\
+file_in every :::i::i u ($1+L):2 w p pt 7 ps si lc 1 lw wi t ''
+
+
+set terminal qt
+replot
+reset
+
+#, \"output_p.dat" every :::i::i w lp t 'Python'
---
+++ play/main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import scipy.integrate
+import numpy
+import math
+
+n = 400
+eps = 0.5
+M = 10
+L = 1
+t1 = 4.0
+
+def f(z, t):
+    cos = math.cos
+    sin = math.sin
+    cosh = math.cosh
+    sinh = math.sinh
+    pi = math.pi
+    x = z[:n]
+    y = z[n:]
+    fx = numpy.zeros(n)
+    fy = numpy.zeros(n)
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                continue
+            dx = x[i] - x[j]
+            dy = y[i] - y[j]
+            den = cosh(2*pi*dy) - cos(2*pi*dx) + eps*eps
+            fx[i] -= sinh(2*pi*dy)/den
+            fy[i] += sin(2*pi*dx)/den
+    return numpy.hstack((fx, fy))/(2.0*n)
+
+x0 = numpy.linspace(0, L, n)
+x = x0 + 0.01 * numpy.sin(2 * math.pi * x0)
+y = -0.01 * numpy.sin(2 * math.pi * x0)
+z0 = numpy.hstack((x, y))
+t0 = 0
+t = numpy.linspace(t0, t1, M)
+z, info = scipy.integrate.odeint(f, z0, t, full_output=True)
+x = z[:, :n]
+y = z[:, n:]
+
+for t in range(M):
+    if t > 0:
+        print("")
+    for i in range(n):
+        print(x[t, i], y[t, i])
