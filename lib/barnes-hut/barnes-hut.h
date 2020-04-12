6301a68813aa379da64037513d6d850c3d6486ac
Sergey Litvinov <slitvinov@gmail.com>
2020-04-12 15:20:27 +0200

    barnes-hut: wip

--- lib/barnes-hut/barnes-hut.h
+++ lib/barnes-hut/barnes-hut.h
@@ -2,7 +2,4 @@ struct BarnesHut;
 struct BarnesHut *barnes_hut_ini(double, double, double, double);
 int barnes_hut_fin(struct BarnesHut *);
 int barnes_hut_insert(struct BarnesHut *, double, double, double, long);
-int barnes_hut_build(struct BarnesHut *, long, const double *,
-                     const double *);
-int barnes_hut_rectangle(struct BarnesHut *, double xlo, double xhi,
-                         double ylo, double yhi, long *, const long **);
+int barnes_hut_print(struct BarnesHut *, FILE *);
--- lib/barnes-hut/main.c
+++ lib/barnes-hut/main.c
@@ -25,6 +25,13 @@ struct BarnesHut {
     struct Node **node;
 };
 
+static int quadrant(struct Node *, double, double);
+static int box(struct Node *, int, double *, double *, double *, double *);
+static struct Node *node_ini(struct BarnesHut *);
+static int insert(struct BarnesHut *, struct Node *, double, double,
+                  double, long);
+static int walk(struct BarnesHut *, struct Node *, int (*)(struct BarnesHut *, struct Node *, void *), void *);
+
 struct BarnesHut *
 barnes_hut_ini(double xl, double xh, double yl, double yh)
 {
@@ -59,6 +66,70 @@ barnes_hut_fin(struct BarnesHut *q)
     return 0;
 }
 
+int
+barnes_hut_insert(struct BarnesHut *q, double x, double y, double m,
+                  long id)
+{
+    struct Node *no;
+
+    if (q->cnt == 0) {
+        if ((no = node_ini(q)) == NULL) {
+            fprintf(stderr, "%s:%d: malloc failed\n", __FILE__, __LINE__);
+            return 1;
+        }
+        no->m = m;
+        no->x = x;
+        no->y = y;
+        no->id = id;
+        no->xl = q->xl;
+        no->xh = q->xh;
+        no->yl = q->yl;
+        no->yh = q->yh;
+        no->elm[NE] = NULL;
+        no->elm[NW] = NULL;
+        no->elm[SW] = NULL;
+        no->elm[SE] = NULL;
+    } else {
+        insert(q, q->node[0], x, y, m, id);
+    }
+    return 0;
+}
+
+static int
+print(struct BarnesHut *q, struct Node *n, void *f0)
+{
+    FILE *f;
+    (void)q;
+
+    f = f0;
+
+    if (n->elm[NE] == NULL && n->elm[NW] == NULL
+        && n->elm[SW] == NULL && n->elm[SE] == NULL) {
+        if (fprintf(f, "%.16g %.16g\n", n->xl, n->yl) < 0)
+            return 1;
+        if (fprintf(f, "%.16g %.16g\n", n->xh, n->yl) < 0)
+            return 1;
+        if (fprintf(f, "%.16g %.16g\n", n->xh, n->yh) < 0)
+            return 1;
+        if (fprintf(f, "%.16g %.16g\n", n->xl, n->yh) < 0)
+            return 1;
+        if (fprintf(f, "%.16g %.16g\n", n->xl, n->yl) < 0)
+            return 1;
+        if (fprintf(f, "\n") < 0)
+            return 1;
+    }
+    return 0;
+}
+
+int
+barnes_hut_print(struct BarnesHut *q, FILE *f)
+{
+    if (q->cnt > 0)
+        return walk(q, q->node[0], print, f);
+    else
+        return 0;
+}
+
 static int
 quadrant(struct Node *n, double x, double y)
 {
@@ -74,7 +145,7 @@ quadrant(struct Node *n, double x, double y)
 }
 
 static int
-box(int i, struct Node *n, double *xl, double *xh, double *yl, double *yh)
+box(struct Node *n, int i, double *xl, double *xh, double *yl, double *yh)
 {
     double x;
     double y;
@@ -110,6 +181,30 @@ box(int i, struct Node *n, double *xl, double *xh, double *yl, double *yh)
     return 0;
 }
 
+static struct Node *
+node_ini(struct BarnesHut *q)
+{
+    struct Node *no;
+
+    if ((no = malloc(sizeof *no)) == NULL) {
+        fprintf(stderr, "%s:%d: malloc failed\n", __FILE__, __LINE__);
+        return NULL;
+    }
+    if (q->cnt == q->cap) {
+        q->cap *= 2;
+        if ((q->node = realloc(q->node, q->cap * sizeof *q->node)) == NULL) {
+            fprintf(stderr, "%s:%d: realloc failed\n", __FILE__, __LINE__);
+            return NULL;
+        }
+    }
+    no->elm[NE] = NULL;
+    no->elm[NW] = NULL;
+    no->elm[SW] = NULL;
+    no->elm[SE] = NULL;
+    q->node[q->cnt++] = no;
+    return no;
+}
+
 static int
 insert(struct BarnesHut *q, struct Node *root, double x, double y,
        double m, long id)
@@ -117,77 +212,57 @@ insert(struct BarnesHut *q, struct Node *root, double x, double y,
     int i;
     struct Node *no;
 
-    if ((no = malloc(sizeof *no)) == NULL) {
-        fprintf(stderr, "%s:%d: malloc failed\n", __FILE__, __LINE__);
-        return 1;
-    }
-    if (q->cnt == q->cap) {
-        q->cap *= 2;
-        if ((q->node = realloc(q->node, q->cap * sizeof *q->node)) == NULL) {
-            fprintf(stderr, "%s:%d: realloc failed\n", __FILE__, __LINE__);
-            return 1;
-        }
-    }
-    no->elm[NE] = NULL;
-    no->elm[NW] = NULL;
-    no->elm[SW] = NULL;
-    no->elm[SE] = NULL;
-    no->m = m;
-    no->id = id;
-    q->node[q->cnt++] = no;
     if (root->elm[NE] == NULL && root->elm[NW] == NULL
         && root->elm[SW] == NULL && root->elm[SE] == NULL) {
         i = quadrant(root, root->x, root->y);
+        if ((no = node_ini(q)) == NULL) {
+            fprintf(stderr, "%s:%d: node_ini failed\n", __FILE__,
+                    __LINE__);
+            return 1;
+        }
+        no->m = root->m;
+        no->x = root->x;
+        no->y = root->y;
+        no->id = root->id;
+        box(root, i, &no->xl, &no->xh, &no->yl, &no->yh);
         root->elm[i] = no;
-        box(i, root, &no->xl, &no->xh, &no->yl, &no->yh);
-        /* */
+        insert(q, root, x, y, m, id);
     } else {
+        i = quadrant(root, x, y);
+        if (root->elm[i] == NULL) {
+            if ((no = node_ini(q)) == NULL) {
+                fprintf(stderr, "%s:%d: node_ini failed\n", __FILE__,
+                        __LINE__);
+                return 1;
+            }
+            no->m = m;
+            no->x = x;
+            no->y = y;
+            no->id = id;
+            box(root, i, &no->xl, &no->xh, &no->yl, &no->yh);
+            root->elm[i] = no;
+        } else
+            insert(q, root->elm[i], x, y, m, id);
+        root->x = (root->x + x * m) / (root->m + m);
+        root->y = (root->y + y * m) / (root->m + m);
     }
     return 0;
 }
 
-int
-barnes_hut_insert(struct BarnesHut *q, double x, double y, double m,
-                  long id)
+static int
+walk(struct BarnesHut *q, struct Node *node, int (*fun)(struct BarnesHut *, struct Node *, void *data), void *data)
 {
-    double xh;
-    double xl;
-    double yh;
-    double yl;
-    long cap;
-    long cnt;
-    struct Node **node;
-    struct Node *no;
-
-    xl = q->xl;
-    xh = q->xh;
-    yl = q->yl;
-    yh = q->yh;
-    cap = q->cap;
-    cnt = q->cnt;
-    node = q->node;
-    if (cnt == 0) {
-        if ((no = malloc(sizeof *no)) == NULL) {
-            fprintf(stderr, "%s:%d: malloc failed\n", __FILE__, __LINE__);
+    if (node != NULL) {
+        if (fun(q, node, data) != 0)
             return 1;
-        }
-        no->m = m;
-        no->x = x;
-        no->y = y;
-        no->id = id;
-        no->xl = xl;
-        no->xh = xh;
-        no->yl = yl;
-        no->yh = yh;
-        no->elm[NE] = NULL;
-        no->elm[NW] = NULL;
-        no->elm[SW] = NULL;
-        no->elm[SE] = NULL;
-        node[cnt++] = no;
-    } else
-        insert(q, node[0], x, y, m, id);
-    q->cap = cap;
-    q->cnt = cnt;
-    q->node = node;
+        if (walk(q, node->elm[NE], fun, data) != 0)
+            return 1;
+        if (walk(q, node->elm[SE], fun, data) != 0)
+            return 1;
+        if (walk(q, node->elm[NW], fun, data) != 0)
+            return 1;
+        if (walk(q, node->elm[SW], fun, data) != 0)
+            return 1;        
+    }
     return 0;
 }
