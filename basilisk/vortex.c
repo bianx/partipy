04c86c0864bb76fbebfb24dea6f3ad22325d407b
Sergey Litvinov <slitvinov@gmail.com>
2020-04-22 22:55:03 +0200

    upd

--- basilisk/vortex.c
+++ basilisk/vortex.c
@@ -1,21 +1,27 @@
+#include "view.h"
 #include "navier-stokes/centered.h"
 
-#define MAXLEVEL (8)
+#define MAXLEVEL (7)
 
 uf.n[left]   = 0.;
 uf.n[right]  = 0.;
 uf.n[top]    = 0.;
 uf.n[bottom] = 0.;
 
+static const char *me = "vertex";
+static double contours[] = {0.25, 0.50, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+
 static double
-f(double z, double q) {
+f(double z, double q)
+{
     double eps;
-    eps = 1e-6;
+    eps = 1e-12;
     return fabs(z) < eps ? 0 : exp(-(q/z)*exp(1/(z - 1)));
 }
 
 static double
-vorI(double x, double y) {
+vorI(double x, double y)
+{
     double a;
     double b;
     double Ksi;
@@ -52,7 +58,6 @@ event init (t = 0)
         psi[] = 0.;
     }
     boundary ({psi,omega});
-
     poisson (psi, omega);
     coord f = {-1.,1.};
     foreach()
@@ -61,24 +66,37 @@ event init (t = 0)
     boundary ((scalar *){u});
 }
 
-event logfile (t <= 24; t += 1) {
+event logfile (t <= 24; t += 1)
+{
     scalar omega[];
     vorticity (u, omega);
     stats s = statsf (omega);
     fprintf (ferr, "%g %d %g %g %g %d\n", t, i, dt, s.sum, s.max, mgp.i);
 }
 
-event output (t += 0.5) {
-    static int nf = 0;
+event output (t += 0.5)
+{
+    enum {SIZE = 99};
+    char name[SIZE];
     scalar omega[];
+    FILE *f;
+    int i;
+    static int nf = 0;
     vorticity (u, omega);
+    snprintf (name, SIZE, "omega-%03d", nf);
+    if ((f = fopen (name, "w"))  == NULL) {
+        fprintf(stderr, "%s: fail to open '%s'\n", me, name);
+        exit(2);
+    }
+    output_field ({omega}, f, linear = true);
+    fclose(f);
 
-    char name[80];
-    sprintf (name, "omega-%03d", nf);
-    FILE * fp = fopen (name, "w");
-    output_field ({omega}, fp, linear = true);
-    fclose (fp);
-
+    view(width = 1200, height = 1200);
+    clear();
+    for (i = 0; i < sizeof contours/sizeof *contours; i++) 
+        isoline ("omega", contours[i], lw = 2);
+    snprintf (name, SIZE, "%03d.png", nf);
+    save(name);
     nf++;
 }
 
@@ -87,4 +105,5 @@ event output (t += 0.5) {
 event adapt (i++) {
     adapt_wavelet ((scalar *){u}, (double[]){5e-5,5e-5}, MAXLEVEL);
 }
-#endif */
+#endif
+*/
