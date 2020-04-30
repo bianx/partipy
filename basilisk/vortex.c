31ff62218702efafd83756c6df65ccc58caa68eb
Sergey Litvinov <slitvinov@gmail.com>
2020-04-30 02:02:02 +0200

    add optision

--- basilisk/vortex.c
+++ basilisk/vortex.c
@@ -1,14 +1,119 @@
 #include "view.h"
 #include "navier-stokes/centered.h"
 
-#define MAXLEVEL (6)
+#define MAXLEVEL (9)
 static const char *me = "vertex";
-uf.n[left]   = 0.;
-uf.n[right]  = 0.;
-uf.n[top]    = 0.;
-uf.n[bottom] = 0.;
 static double contours[] = {0.25, 0.50, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-static const double (*vor)(double, double);
+static double (*vor)(double, double);
+static double vorI(double, double);
+static double vorII(double, double);
+static double f(double, double);
+
+static void
+usg(void)
+{
+    fprintf(stderr, "%s [-1|-2]\n", me);
+    exit(2);
+}
+
+uf.n[left]   = 0;
+uf.n[right]  = 0;
+uf.n[top]    = 0;
+uf.n[bottom] = 0;
+
+u.n[left] = neumann(0);
+u.n[right] = neumann(0);
+u.n[top] = neumann(0);
+u.n[bottom] = neumann(0);
+
+int main(int argc, char **argv)
+{
+    int Vflag;
+    (void)argc;
+    Vflag = 0;
+    while (*++argv != NULL && argv[0][0] == '-')
+        switch (argv[0][1]) {
+        case 'h':
+            usg();
+            break;
+        case '1':
+            vor = vorI;
+            break;
+        case '2':
+            vor = vorII;
+            break;
+        default:
+            fprintf(stderr, "%s: unknown option '%s'\n", me, argv[0]);
+            exit(2);
+        }
+    if (!Vflag) {
+        fprintf(stderr, "%s: -1 or -2 must be given\n", me);
+        exit(2);
+    }
+    origin(-4, -4);
+    size(8);
+    init_grid(1 << MAXLEVEL);
+    run();
+}
+
+event init (t = 0)
+{
+    scalar psi[];
+    scalar omega[];
+
+    psi[left]   = dirichlet(0);
+    psi[right]  = dirichlet(0);
+    psi[top]    = dirichlet(0);
+    psi[bottom] = dirichlet(0);
+    foreach() {
+        omega[] = vor(x, y);
+        psi[] = 0;
+    }
+    boundary({psi,omega});
+    poisson(psi, omega);
+    coord f = {-1.,1.};
+    foreach()
+        foreach_dimension()
+        u.x[] = f.x*(psi[0,1] - psi[0,-1])/(2.*Delta);
+    boundary((scalar *){u});
+}
+
+event logfile (t <= 24; t += 1)
+{
+    scalar omega[];
+    stats s;
+    vorticity(u, omega);
+    s = statsf(omega);
+    fprintf(stderr, "%g %d %g %g %g %d\n", t, i, dt, s.sum, s.max, mgp.i);
+}
+
+event output (t += 0.5)
+{
+    enum {SIZE = 99};
+    char name[SIZE];
+    scalar omega[];
+    FILE *f;
+    int i;
+    static int nf = 0;
+
+    vorticity (u, omega);
+    snprintf (name, SIZE, "omega-%03d", nf);
+    if ((f = fopen (name, "w"))  == NULL) {
+        fprintf(stderr, "%s: fail to open '%s'\n", me, name);
+        exit(2);
+    }
+    output_field ({omega}, f, linear = true);
+    fclose(f);
+
+    view(width = 1200, height = 1200, fov = 10);
+    clear();
+    for (i = 0; i < sizeof contours/sizeof *contours; i++)
+        isoline ("omega", contours[i], lw = 2);
+    snprintf (name, SIZE, "%03d.png", nf);
+    save(name);
+    nf++;
+}
+
 static double
 f(double z, double q)
 {
@@ -47,71 +152,6 @@ vorII(double x, double y)
     Ksi = 20;
     r2 = sq(x/a) + sq(y/b);
     return r2 <= 1 ? Ksi * (1 - r2 * r2) : 0;
-}
-
-int main()
-{
-    vor = vorI;
-    origin (-2, -2);
-    size(8);
-    init_grid (1 << MAXLEVEL);
-    run();
-}
-
-event init (t = 0)
-{
-    scalar psi[], omega[];
-
-    psi[left]   = dirichlet(0);
-    psi[right]  = dirichlet(0);
-    psi[top]    = dirichlet(0);
-    psi[bottom] = dirichlet(0);
-
-    foreach() {
-        omega[] = vor(x, y);
-        psi[] = 0.;
-    }
-    boundary ({psi,omega});
-    poisson (psi, omega);
-    coord f = {-1.,1.};
-    foreach()
-        foreach_dimension()
-        u.x[] = f.x*(psi[0,1] - psi[0,-1])/(2.*Delta);
-    boundary ((scalar *){u});
-}
-
-event logfile (t <= 24; t += 1)
-{
-    scalar omega[];
-    vorticity (u, omega);
-    stats s = statsf (omega);
-    fprintf (ferr, "%g %d %g %g %g %d\n", t, i, dt, s.sum, s.max, mgp.i);
-}
-
-event output (t += 0.5)
-{
-    enum {SIZE = 99};
-    char name[SIZE];
-    scalar omega[];
-    FILE *f;
-    int i;
-    static int nf = 0;
-    vorticity (u, omega);
-    snprintf (name, SIZE, "omega-%03d", nf);
-    if ((f = fopen (name, "w"))  == NULL) {
-        fprintf(stderr, "%s: fail to open '%s'\n", me, name);
-        exit(2);
-    }
-    output_field ({omega}, f, linear = true);
-    fclose(f);
-
-    view(width = 1200, height = 1200);
-    clear();
-    for (i = 0; i < sizeof contours/sizeof *contours; i++) 
-        isoline ("omega", contours[i], lw = 2);
-    snprintf (name, SIZE, "%03d.png", nf);
-    save(name);
-    nf++;
 }
 
 /*
