8059bd605f47bb5eb6ca1ad03162a28995ed5900
Sergey Litvinov <slitvinov@gmail.com>
2020-05-20 19:05:51 +0200

    initial import of lib/tree: a copy of lib/barnes-hut

---
+++ lib/tree/tree.h
@@ -0,0 +1,26 @@
+struct Tree;
+
+struct Tree *tree_ini(double, double, double);
+struct Tree *tree_build(long, const double *, const double *, const double *);
+
+int tree_fin(struct Tree *);
+int tree_insert(struct Tree *, double, double, double, long);
+
+int tree_print(struct Tree *, FILE *);
+int tree_interaction(struct Tree *q, double theta,
+                           long id, double x, double y, long *cnt,
+                           double *px, double *py, double *m);
+struct TreeInfo {
+    double m;
+    double mx;
+    double my;
+    double w;
+    double x;
+    double y;
+    int Leaf;
+    int Coarse;
+    long id;
+};
+int tree_info(struct Tree *q, double theta,
+                    long id, double x, double y, long *cnt,
+                    struct TreeInfo *info);
